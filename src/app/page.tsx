13cf9cc7648a07b620b3cb56f9827119879a7eea
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-11-08 16:45:52 +0000

    section above jumplinks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <section className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40">
+        <p className="text-2xl">Section above jumplinks</p>
+      </section>
     </main>
   )
 }
