ad73ae7da4a5e4ad625cea27613689cf6d09d474
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-11-09 13:14:07 +0000

    figuring out how to use intersection obsserver

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,57 @@
-import { useEffect, useState } from 'react';
+'use client';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export default function Home() {
   const [sections, setSections] = useState([]);
+  const [links, setLinks] = useState([]);
 
   const createSections = () => {
     // create 5 sections and 5 links
-    const links = [];
-    useEffect(()=>{  
-    const sectionElements = [];
-    for (let i = 1; i <= 5; i++) {
-      const id = `section-${i}`;
 
-      const section = (
-        <section id={id} key={i} className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40 my-5">
-          <p className="text-2xl">Section {i}</p>
-        </section>
-      );     
-       sectionElements.push(section);
-
-    }
-    setSections(sectionElements)},[]);
-  
-      links.push(
-        <a href={`#${id}`} key={i}>Section {i} </a>
-      )
-    }
-    // update state variables
-    const [currentSection, setCurrentSection] = useState(null);
-    // set up intersection observer and useEffect hook to directly manipulate the state variable
     useEffect(() => {
-      const observer = new IntersectionObserver(entries => {
-        const visibleSection = entries.find(entry => entry.isIntersecting);
-        setCurrentSection(visibleSection.target.id);
-      });
-      sections.forEach(section => observer.observe(section));
-      return () => observer.disconnect();
-    }, [sections])
+
+      let newSection = [];
+     let newLink = [];
+
+      for (let i = 1; i <= 5; i++) {
+        const id = `section-${i}`;
+
+        const section = (
+          <section id={id} key={i} className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40 my-5">
+            <p className="text-2xl">Section {i}</p>
+          </section>
+        );
+
+        const link = (
+          <a href={`#${id}`} key={i}>Section {i} </a>
+        );
+
+        newSection.push(section);
+        newLink.push(link);
+      }
+      setSections(newSection);
+      setLinks(newLink);
+    }, []);
 
     return {
       sections, links
     }
 
   }
-  const { sections, links } = createSections();
+  createSections();
   // loop 5 times to generate a link for each section
+
+
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <section className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40">
+      <header className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40">
         <p className="text-2xl">Section above jumplinks</p>
-      </section>
+      </header>
       {/* sticky nav bar here */}
       <nav className="sticky top-0 flex flex-row justify-between items-center min-w-[100vw] h-16 bg-zinc-950 text-white">
-        {currentSection}
+        {setLinks}
       </nav>
       {/* sections here */}
       <div>
