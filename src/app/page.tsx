4423fe3e02ec0d700dfb2b50de7c685a31342289
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-11-09 12:39:33 +0000

    trying to fix bug with sections variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,33 @@
 import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [sections, setSections] = useState([]);
 
   const createSections = () => {
-    const sections = [];
+    // create 5 sections and 5 links
     const links = [];
+    useEffect(()=>{  
+    const sectionElements = [];
     for (let i = 1; i <= 5; i++) {
       const id = `section-${i}`;
 
-      sections.push(
+      const section = (
         <section id={id} key={i} className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40 my-5">
           <p className="text-2xl">Section {i}</p>
         </section>
-      )
+      );     
+       sectionElements.push(section);
+
+    }
+    setSections(sectionElements)},[]);
+  
       links.push(
         <a href={`#${id}`} key={i}>Section {i} </a>
       )
     }
+    // update state variables
     const [currentSection, setCurrentSection] = useState(null);
+    // set up intersection observer and useEffect hook to directly manipulate the state variable
     useEffect(() => {
       const observer = new IntersectionObserver(entries => {
         const visibleSection = entries.find(entry => entry.isIntersecting);
