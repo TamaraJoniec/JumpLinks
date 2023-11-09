45f09466d5b9793e5c9518c60ff8da1f6f3e3924
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-11-09 11:01:19 +0000

    create function to recreate sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,19 @@
 import Link from 'next/link'
-import Script from 'next/script'
-import Sections from './components/sections'
 
-type Props = {
-  title: string;
-  i?: number;
-};
+export default function Home( {title} : {title: string  }) {
 
-export default function Home({ title, i }: Props) {
-
-  const createSection = ({ title, i }: Props) => {
-    for (let i = 0; i < 5; i++) {
-      <section id={`"section ${i}"`} className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40 my-5">
-        <p className="text-2xl">{title}{i}</p>
-      </section>
+  const createSections = () => {
+    let sections = [];
+    for (let i = 1; i <= 5; i++) {
+      sections.push(
+        <section id={`"section-${i}"`} key={i}  className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40 my-5">
+          <p className="text-2xl">{title} {i}</p>
+        </section>
+      )
+        return sections;
     }
 
   }
-  createSection({ title, i });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <section className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40">
@@ -36,13 +32,8 @@ export default function Home({ title, i }: Props) {
 
         </div>
       </nav>
-      {/* 
-      <Sections title="Section 1" id="section 1" />
-      <Sections title="Section 2" id="section 2" />
-      <Sections title="Section 3" id="section 3" />
-      <Sections title="Section 4" id="section 4" />
-      <Sections title="Section 5" id="section 5" /> */}
-
+      {/* sections here */}
+      {createSections()}
     </main>
   )
 }
