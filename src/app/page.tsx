ccb538087edd05651348ef328a47916635270bb7
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-11-09 10:53:30 +0000

    figuring out for loop to recreate sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,20 @@ import Sections from './components/sections'
 
 type Props = {
   title: string;
-  id: string;
   i?: number;
 };
 
-export default function Home({title, id, i}:Props) {
+export default function Home({ title, i }: Props) {
+
+  const createSection = ({ title, i }: Props) => {
+    for (let i = 0; i < 5; i++) {
+      <section id={`"section ${i}"`} className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40 my-5">
+        <p className="text-2xl">{title}{i}</p>
+      </section>
+    }
+
+  }
+  createSection({ title, i });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <section className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40">
@@ -16,31 +25,23 @@ export default function Home({title, id, i}:Props) {
       </section>
       {/* sticky nav bar here */}
 
-      <nav className="sticky top-0 flex flex-row justify-between items-center min-w-[100vw] h-16 bg-zinc-950">
-            <div className="flex flex-row justify-between items-center">
-                <Script type="ts">
-                    <ul>
-                        // make navbar links to sections
-                        <li>
-                            <Link href="#">{title}</Link>
-                        </li>
-                    </ul>
-                </Script>
-                {/* drop down navigation bar */}
+      <nav className="sticky top-0 flex flex-row justify-between items-center min-w-[100vw] h-16 bg-zinc-950 text-white">
+        <div className="flex flex-row justify-between items-center">
+          <Link href="#Section-1">{title}</Link>
+          <Link href="#Section-2">{title}</Link>
+          <Link href="#Section-3">{title}</Link>
+          <Link href="#Section-4">{title}</Link>
+          <Link href="#Section-5">{title}</Link>
+          {/* drop down navigation bar */}
 
-            </div>
-        </nav>
-        <Script>  
-          <section id={`"section ${i}"`} className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40 my-5">
-            <p className="text-2xl">{title}{i}</p>
-        </section>
-        </Script>
-      
+        </div>
+      </nav>
+      {/* 
       <Sections title="Section 1" id="section 1" />
       <Sections title="Section 2" id="section 2" />
       <Sections title="Section 3" id="section 3" />
       <Sections title="Section 4" id="section 4" />
-      <Sections title="Section 5" id="section 5" />
+      <Sections title="Section 5" id="section 5" /> */}
 
     </main>
   )
