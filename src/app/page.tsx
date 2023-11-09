37a438893d1550a8576da8988c707948241af23b
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-11-09 11:47:37 +0000

    for loop for 5 nav links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-import Link from 'next/link'
 
 export default function Home({ title }: { title: string }) {
 
+  // loop 5 times to generate a link for each section
+  const links = [];
+  for (let i=1; i <=5; i++){
+    links.push(
+      <a href={`#section-${i}`} key={i}>Section {i} </a>
+    )
+  }
   const createSections = () => {
     let sections = [];
     for (let i = 1; i <= 5; i++) {
       sections.push(
         <section id={`"section-${i}"`} key={i} className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40 my-5">
-          <p className="text-2xl">{title} {i}</p>
+          <p className="text-2xl">Section {i}</p>
         </section>
       )
     } 
@@ -24,11 +30,7 @@ export default function Home({ title }: { title: string }) {
 
       <nav className="sticky top-0 flex flex-row justify-between items-center min-w-[100vw] h-16 bg-zinc-950 text-white">
         <div className="flex flex-row justify-between items-center">
-          <Link href="#Section-1">{title}</Link>
-          <Link href="#Section-2">{title}</Link>
-          <Link href="#Section-3">{title}</Link>
-          <Link href="#Section-4">{title}</Link>
-          <Link href="#Section-5">{title}</Link>
+          {links}
           {/* drop down navigation bar */}
 
         </div>
