368006ac720acaee22750717155df635794ce725
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-11-09 11:55:59 +0000

    unique IDs to create links, returned both sections and links to render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
 
-export default function Home({ title }: { title: string }) {
+export default function Home() {
 
-  // loop 5 times to generate a link for each section
-  const links = [];
-  for (let i=1; i <=5; i++){
-    links.push(
-      <a href={`#section-${i}`} key={i}>Section {i} </a>
-    )
-  }
   const createSections = () => {
     let sections = [];
+    const links = [];
     for (let i = 1; i <= 5; i++) {
+
+      const id = `section-${i}`;
+
       sections.push(
-        <section id={`"section-${i}"`} key={i} className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40 my-5">
+        <section id={id} key={i} className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40 my-5">
           <p className="text-2xl">Section {i}</p>
         </section>
       )
-    } 
-    return sections;
-  }      
-  const sections = createSections();
+      links.push(
+        <a href={`#${id}`} key={i}>Section {i} </a>
+      )
+    }
+    return {
+      sections, links
+    }
+
+  }
+  const { sections, links } = createSections();
+  // loop 5 times to generate a link for each section
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
@@ -27,19 +31,13 @@ export default function Home({ title }: { title: string }) {
         <p className="text-2xl">Section above jumplinks</p>
       </section>
       {/* sticky nav bar here */}
-
       <nav className="sticky top-0 flex flex-row justify-between items-center min-w-[100vw] h-16 bg-zinc-950 text-white">
-        <div className="flex flex-row justify-between items-center">
-          {links}
-          {/* drop down navigation bar */}
-
-        </div>
+        {links}
       </nav>
       {/* sections here */}
       <div>
         {sections}
       </div>
-    
     </main>
   )
 }
