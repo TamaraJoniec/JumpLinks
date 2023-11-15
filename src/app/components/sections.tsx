3181cc7fc15d8eeef781364f3d55a417baea235c
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-11-15 10:39:49 +0000

    added background images to each section with a dark overlay for readbility

--- src/app/components/sections.tsx
+++ src/app/components/sections.tsx
@@ -1,20 +1,24 @@
 'use client'
 
 type Props = {
-    section: {title: string, description: string};
-    i?: number;
-  };
+  section: { title: string, description: string, image: string };
+  i?: number;
+};
 
-export default function Sections ({section, i}:Props) {
-    return (
-        <section
-        id={`section-${i}`}
-        data-title={section.title}
-        data-index={i}
-        key={`section-${i}`}
-        className="min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40 my-5">
-        <h3 className="py-20 text-2xl">{section.title}</h3>
-        <p>{section.description}</p>
-      </section>
-    )
+export default function Sections({ section, i }: Props) {
+  return (
+    <section
+      id={`section-${i}`}
+      data-title={section.title}
+      data-index={i}
+      key={`section-${i}`}
+      style={{ backgroundImage: `url(${section.image})`, backgroundSize: `cover`, backgroundPosition: `center`, backgroundRepeat: `no-repeat` }}
+      className="min-h-[75vh] min-w-[100vw] bg-slate-300 z-0 relative my-10">
+      <div className="bg-black opacity-70 z-1 min-h-[75vh] min-w-[100vw] absolute py-10 px-40"></div>
+      <div className="absolute z-2">
+        <h3 className="p-20 text-2xl text-white">{section.title}</h3>
+        <p className="p-20 text-xl text-white">{section.description}</p>
+      </div>
+    </section>
+  )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,29 +8,29 @@ export default function Home() {
   const [sections] = useState([...Data.sections]);
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
-      const topEntry = entries.reduce((prev, current) => 
+      const topEntry = entries.reduce((prev, current) =>
         (prev.intersectionRatio > current.intersectionRatio) ? prev : current
       );
 
       if (topEntry.isIntersecting) {
-        if (topEntry.target.dataset.title){
+        if (topEntry.target.dataset.title) {
           setCurrentSection(topEntry.target.dataset.title);
         }
       }
-    }, { threshold: [0.1] }); 
+    }, { threshold: [0.1] });
 
     document.querySelectorAll('section').forEach(element => {
       observer.observe(element);
     });
-    return () => observer.disconnect(); 
+    return () => observer.disconnect();
   }, []);
   return (
     <div>
-      <header className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40  bg-auto bg-no-repeat bg-center" style={{backgroundImage: `url('https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/United%20Kingdom/Yorkshire/Leeds/Leeds-lights-roads-Getty.jpg')`, }}>
+      <header className=" min-h-[75vh] min-w-[100vw] bg-slate-300 py-10 px-40  bg-auto bg-no-repeat bg-center" style={{ backgroundImage: `url('https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/United%20Kingdom/Yorkshire/Leeds/Leeds-lights-roads-Getty.jpg')`, }}>
       </header>
-      <nav className="sticky top-0 flex flex-row flex-wrap  bg-zinc-950">
+      <nav className="sticky z-50 top-0 flex flex-row flex-wrap  bg-zinc-950">
         <div className=" bg-zinc-950 min-w-[100vw] grid grid-rows-1 text-white">
-    <Dropdown className="sticky right-0"
+          <Dropdown className="sticky right-0"
             setCurrentSection={setCurrentSection}
             sections={sections}
             currentSection={currentSection} >
@@ -39,12 +39,12 @@ export default function Home() {
       </nav>
 
       <main className="flex min-h-screen flex-col items-center justify-between p-10">
-          
+
         <div key="sections">
           {sections &&
             sections.map((section, i) => {
               return (
-                <Sections section={section} i={i} key={i}  />
+                <Sections section={section} i={i} key={i} />
               )
             })}
         </div>
